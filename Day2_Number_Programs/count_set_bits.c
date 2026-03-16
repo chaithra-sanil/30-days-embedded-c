739bdfcf01ca5e9580795dde6f1218d7f1119d87
chaithra-sanil <chaithranarayanapoojary@gmail.com>
2026-03-16 23:08:24 +0530

    Day-3 count set bits

--- Day2_Number_Programs/count_set_bits.c
+++ Day2_Number_Programs/count_set_bits.c
@@ -1,13 +1,29 @@
+// #include <stdio.h>
+// int main(){
+//     int num,count=0;
+//     printf("Enter the number:");
+//     scanf("%d", &num);
+//     while(num != 0){
+//         if(num &1){
+//             count ++;
+//         }
+//         num = num << 1;
+//     }
+//     printf("Number of set bits are: %d\n", count);
+// }
+
+
+//METHOD 2
+
 #include <stdio.h>
 int main(){
-    int num,count=0;
-    printf("Enter the number:");
+    int num, count=0;
+    printf("Enter the numbe: ");
     scanf("%d", &num);
-    while(num > 0){
-        if(num &1){
-            count ++;
-        }
-        num = num >> 1;
+    while(num){
+        num = num & (num-1);
+        count ++;
     }
-    printf("Number of set bits are: %d\n", count);
+    printf("Number of set bits are %d", count);
 }
+
