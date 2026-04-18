d2101ef8464c246c9b16bd06012638d0108011c8
chaithra-sanil <chaithranarayanapoojary@gmail.com>
2026-04-18 12:11:24 +0530

    check bit

--- DayBitManipulation/check_bit.c
+++ DayBitManipulation/check_bit.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/* #include <stdio.h>
 int main(){
     int num, pos, result;
     printf("Enter the number and position:");
@@ -9,4 +9,21 @@ int main(){
     }else{
         printf("bit is unset in the pos %d", pos);
     }
+} */
+
+/* Given an array find the xor of all the elements where the number in that ithbit is set 
+array= [2,4,10,1,3] and result should be 1^3 , ith pos is 0th*/
+
+#include <stdio.h>
+int main(){
+
+    int arr[5]={2,4,10,1,3};
+    int result, pos=0,i, xoresult=0;
+    for(i=0;i<5;i++){
+        result = (arr[i] & (1<<pos));
+        if(result >0){
+            xoresult ^= arr[i];
+        }
+    }
+    printf("%d",xoresult);
 }
